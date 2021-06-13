e026a930abc8c4b09d2a6f6b3c617f9ff1cf3be1
KubaWis128 <j2006wis1@gmail.com>
2021-06-13 21:16:17 +0200

    Uzywa teraz modulu zamiast pliku .js

--- preload.js
+++ preload.js
@@ -2,6 +2,9 @@
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+// Option 1: Import the entire three.js core library.
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
